412aaf6bf6458be1582bf83c459f01a4a3d0c896
DivyanshSuwalka <suwalkadivyansh63@gmail.com>
2025-04-08 22:23:18 +0530

    authentication update

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
 
-const authentication = (req, res, next) => {
+const authentication = async (req, res, next) => {
   const token = req.cookies.token;
   if (!token) return res.status(401).json({ message: "Access Denied" });
 
   try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    const verified = await jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     next();
   } catch (error) {
